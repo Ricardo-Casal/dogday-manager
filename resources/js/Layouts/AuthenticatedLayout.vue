684b24877b1c2f5c9e20bd44d2bbcf57066a863a
Ricardo-Casal <ricardo.casal@hotmail.com>
2026-04-14 17:11:11 +0100

    Fix logo link to redirect owners to owner dashboard

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -22,7 +22,7 @@ const showingNavigationDropdown = ref(false);
                         <div class="flex">
                             <!-- Logo -->
                             <div class="flex shrink-0 items-center">
-                                <Link :href="route('dashboard')">
+                                <Link :href="$page.props.auth.role === 'staff' ? route('dashboard') : route('owner.dashboard')">
                                     <ApplicationLogo
                                         class="block h-9 w-auto fill-current text-gray-800"
                                     />
